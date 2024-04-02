8e251b95669ca0006695c1aa8959c37f709b118a
fmatzy <30208309+fmatzy@users.noreply.github.com>
2024-04-03 00:30:35 +0900

    add prettier for tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,11 +9,11 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-[system-ui,sans-serif] leading-8 mx-4 my-2">
+    <div className="mx-4 my-2 font-[system-ui,sans-serif] leading-8">
       <h1 className="text-2xl font-bold text-blue-500">
         Welcome to Remix (SPA Mode)
       </h1>
-      <ul className="list-disc list-inside">
+      <ul className="list-inside list-disc">
         <li>
           <a
             target="_blank"
